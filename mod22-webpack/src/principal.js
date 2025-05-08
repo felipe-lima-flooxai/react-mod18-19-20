ec42da1ece239e6897c7b4ac52e68337c4e941e1
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-05-08 13:07:36 -0300

    feat: webpack dev server mt legal

--- mod22-webpack/src/principal.js
+++ mod22-webpack/src/principal.js
@@ -1,5 +1,6 @@
 const Pessoa = require("./pessoa")
 import "./modulos/moduloA.js"
+import "./assets/css/estilo.css"
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- mod22-webpack/webpack.config.js
+++ mod22-webpack/webpack.config.js
@@ -1,6 +1,34 @@
 const webpack = require("webpack")
+const path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: "./src/principal.js"
+    entry: "./src/principal.js",
+    output: {
+        filename: "principal.js",
+        path: __dirname + "/public"
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, "public"),
+        },
+        port: 9000,
+        hot: true,
+        open: true,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.js$/, // Para arquivos .js
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader', // Requer babel-loader e presets
+            },
+          },
+          {
+            test: /\.css$/, // Para arquivos .css
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
 }
