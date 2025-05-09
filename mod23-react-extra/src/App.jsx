8a9a9cb713842f73ba387787eb2ee17aab593445
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-05-09 11:24:58 -0300

    feat: basico de componentes

---
+++ mod23-react-extra/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import Primeiro from './componentes/Primeiro'
+import Segundo from './componentes/Segundo'
+import { BoaNoite, BoaTarde } from './componentes/Multiplos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Ham tomanocu
+        <Primeiro></Primeiro>
+        <Segundo nome='Rodrigo'></Segundo>
+        <BoaTarde apelido="Chupeta de baleia"></BoaTarde>
+        <BoaNoite apelido='O mais Sagaz'></BoaNoite>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
